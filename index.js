586bdd511f0d25c710e9c2dcaa05bbdbf4bf9da6
amarsharmamrj <amarsharmamrj@gmail.com>
2023-01-25 22:15:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+const app = express();
+
+// middleware
+app.use(express.json())
+
+// cors
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+    methods: "*"
+}
+app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Cross-Origin-Resource-Policy', 'cross-origin');
+    next();
+});
+
+const PORT = process.env.port || 10000
+
+app.get("/home", (req, res) => {
+    res.send("<h1>home api</h1>")
+})
+
+app.listen(PORT, () => {
+    console.log("App is running on the port:", PORT)
+})
