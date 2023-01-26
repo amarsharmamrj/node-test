e31e1ff8e8339945faaea3670b94c3046193b0a2
amarnath vishwakarma <52070562+amarsharmamrj@users.noreply.github.com>
2023-01-26 09:53:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.use(function (req, res, next) {
     next();
 });
 
-const PORT = process.env.PORT || 10000
+const port = process.env.PORT || 3000
 
 app.get("/home", (req, res) => {
     res.send("<h1>home api</h1>")
 })
 
-app.listen(PORT, () => {
-    console.log("App is running on the port:", PORT)
+app.listen(port, () => {
+    console.log("App is running on the port:", port)
 })
